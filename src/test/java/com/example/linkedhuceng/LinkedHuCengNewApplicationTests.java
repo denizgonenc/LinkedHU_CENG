c5668cf33144c7a9d2c3133a4115edf6a34c42c0
Deniz Gönenç <56702717+denizgonenc@users.noreply.github.com>
2023-04-01 18:45:29 +0300

    Add files via upload

---
+++ src/test/java/com/example/linkedhuceng/LinkedHuCengNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.linkedhuceng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedHuCengNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
